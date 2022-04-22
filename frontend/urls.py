d774ec5eeed590d731179ec57836bb9006014f06
mubasharazad12 <mubasharazad3@gmail.com>
2022-04-22 20:54:47 +0500

    Frond end lastest changes

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path,include
 from frontend import views
@@ -5,7 +6,11 @@ from frontend import views
 urlpatterns = [
 
     path('', views.index, name="home"),
-    path('playback', views.playback, name="playback"),
-    path('videoplayer', views.videoplayer, name="videoplayer")
+    path('videos', views.videos, name="videos"),
+    path("channels", views.channels, name="channels"),
+    path('videoplayer', views.videoplayer, name="videoplayer"),
+    path('login', views.login, name="login"),
+    path('register', views.register, name="register"),
+    path('profile', views.profile, name="profile")
   
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -7,8 +7,20 @@ def index(request):
     return render(request, "index.html" )
 
 
-def playback(request):
-    return render(request, "playback.html")
+def videos(request):
+    return render(request, "videos.html")
+
+def channels(request):
+    return render(request, "channels.html")
 
 def videoplayer(request):
     return render(request, "videoplayer.html")
+
+def login(request):
+    return render(request, "accounts/login.html")
+
+def register(request):
+    return render(request, "accounts/register.html")
+
+def profile(rquest):
+    return render(rquest, "profile.html")
