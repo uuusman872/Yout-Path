f249443429c7a1dafaef844515b70abb6af210f7
mubasharazad12 <mubasharazad3@gmail.com>
2022-06-24 16:15:42 +0500

    password validtion addeed

--- backend/settings.py
+++ backend/settings.py
@@ -134,7 +134,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
+
 ]
 
 MEDIA_URL = '/media/'
--- projectApi/forms.py
+++ projectApi/forms.py
@@ -4,14 +4,36 @@ from django.contrib.auth.models import User
 from projectApi.models import ChannelModel, CommentsModel, UserModel, VideoModel
 from django.contrib.auth.password_validation import validate_password
 
+from django.core.exceptions import ValidationError
+import re
+
+
+def validate_password_uppercase(value):
+    if not re.search(r"[A-Z]+", value):
+        raise ValidationError("The password must contain at least one uppercase character.")
+    else:
+        return value
+        
+def validate_password_lowercase(value):
+    if not re.search(r"[a-z]+", value):
+        raise ValidationError("The password must contain at least one lower character.")
+    else:
+        return value
+
+def validate_password_special(value):
+    if not re.search(r"[*@!#%&()^~{}]+", value):
+        raise ValidationError("The password must contain at least one special character.")
+    else:
+        return value
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, validators=[validate_password])
+    password = forms.CharField(widget=forms.PasswordInput, validators=[validate_password, validate_password_uppercase, validate_password_lowercase, validate_password_special])
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'username', 'password']
 
 
+     
 class CreateAccountForm(forms.ModelForm):
     class Meta:
         model = UserModel
